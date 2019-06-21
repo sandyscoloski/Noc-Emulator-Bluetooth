cb0e20e5486849466c066a312428d3637cc38c19
sandyscoloski <39198481+sandyscoloski@users.noreply.github.com>
2019-06-21 16:53:23 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet, Text, View, Button, FlatList,
-        Switch, TouchableOpacity, ToastAndroid } from 'react-native'
-import BluetoothSerial from 'react-native-bluetooth-serial'
+        Switch, TouchableOpacity, ToastAndroid } from 'react-native';
+import BluetoothSerial from 'react-native-bluetooth-serial';
 
 export default class App extends React.Component {
     constructor (props) {
         super (props)
 
         this.state = {
-        isEnabled: false,
-        discovering: false,
-        devices: [],
-        unpairedDevices: [],
-        connected: false,
+            isEnabled: false,
+            discovering: false,
+            devices: [],
+            unpairedDevices: [],
+            connected: false,
         }
     }
 
     discoverAvaliableDevices() {
-        if(this.state.discovering) {
-        return false;
+        if (this.state.discovering) {
+            return false
         }
-        else {
+
         this.setState({ discovering: true })
 
         BluetoothSerial.discoverUnpairedDevices()
@@ -30,27 +30,26 @@ export default class App extends React.Component {
             this.setState({ unpairedDevices: uniqueDevices, discovering: false })
         })
         .catch((err) => console.log(err.message))
-        }
     }
 
     toggleBluetooth(value) {
         if (value === true) {
-        this.enable()
+            this.enable()
         } else {
-        this.disable()
+            this.disable()
         }
     }
 
     enable() {
         BluetoothSerial.enable()
-        .then((res)) => this.setState({ isEnabled: true })
-        .catch((err)) => Toast.showShortBottom(err.message))
+        .then((res) => this.setState({ isEnabled: true }))
+        .catch((err) => Toast.showShortBottom(err.message))
     }
 
     disable() {
         BluetoothSerial.disable()
-        .then((res)) => this.setState({ isEnabled: false })
-        .catch((err)) => Toast.showShortBottom(err.message))
+        .then((res) => this.setState({ isEnabled: false }))
+        .catch((err) => Toast.showShortBottom(err.message))
     }
 
     activeGreenLed() {
@@ -63,12 +62,12 @@ export default class App extends React.Component {
         .catch((err) => console.log(err.message))
     }
 
-    activeRedLed {
+    activeRedLed() {
         BluetoothSerial.write('1')
         .then((res) => {
-        console.log(res)
-        console.log('Successfuly wrote to device')
-        this.setState({ connected: true })
+            console.log(res)
+            console.log('Successfuly wrote to device')
+            this.setState({ connected: true })
         })
         .catch((err) => console.log(err.message))
     }
@@ -78,23 +77,23 @@ export default class App extends React.Component {
 
         BluetoothSerial.connect(device.id)
         .then((res) => {
-        console.log(`Connected to device ${device.name}`)
-        ToastAndroid.show(`Connected to device ${device.name}`, ToastAndroid.SHORT)
+            console.log(`Connected to device ${device.name}`)
+            ToastAndroid.show(`Connected to device ${device.name}`, ToastAndroid.SHORT)
         })
         .catch((err) => console.log((err.message)))
     }
 
-        componentWillMount() {
-            Promise.all([
+    componentWillMount() {
+        Promise.all([
             BluetoothSerial.isEnabled(),
             BluetoothSerial.list()
             ])
-            .then((values) => {
+        .then((values) => {
             const [isEnabled, devices] = values
             this.setState({ isEnabled, devices })
-            })
+        })
 
-            BluetoothSerial.on('bluetoothEnabled', () => {
+        BluetoothSerial.on('bluetoothEnabled', () => {
             Promise.all([
                 BluetoothSerial.isEnabled(),
                 BluetoothSerial.list()
@@ -116,7 +115,7 @@ export default class App extends React.Component {
         return (
             <TouchableOpacity onPress={() => this.connect(item.item)}>
                 <View style={styles.deviceNameWrap}>
-                        <Text style={styles.deviceNameWrap}>
+                        <Text style={styles.deviceName}>
                             { item.item.name ? item.item.name : item.item.id }
                         </Text>
                     </View>
@@ -164,36 +163,35 @@ export default class App extends React.Component {
             </View>
         )
     }
-
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  toolbar: {
-    paddingTop: 30,
-    paddingBottom: 30,
-    flexDirection: 'row'
-  },
-  toolbarButton: {
-    width: 50,
-    marginTop: 8,
-  },
-  tooblarTitle: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 20,
-    flex: 1,
-    marginTop: 6,
-  },
-  deviceName: {
-    fontSize: 17,
-    color: 'black',
-  },
-  deviceNameWrap: {
-    margin: 10,
-    borderBottomWidth: 1,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    toolbar: {
+        paddingTop: 30,
+        paddingBottom: 30,
+        flexDirection: 'row'
+    },
+    toolbarButton: {
+        width: 50,
+        marginTop: 8,
+    },
+    tooblarTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        flex: 1,
+        marginTop: 6,
+    },
+    deviceName: {
+        fontSize: 17,
+        color: 'black',
+    },
+    deviceNameWrap: {
+        margin: 10,
+        borderBottomWidth: 1,
+    }
 })
